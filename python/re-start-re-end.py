9f0b00315c1bbf8bcb074640140a2bd222e1f521
Olcay Çoban <50139250+olcaycoban@users.noreply.github.com>
2020-11-17 17:22:07 +0300

    Update re-start-re-end.py

--- python/re-start-re-end.py
+++ python/re-start-re-end.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
-
 import re
 
-if __name__ == "__main__":
-    string = input()
-    sub = input()
-    
-    matches = list(re.finditer(r'(?={})'.format(sub), string))
-    
+def solve(expressions,pattern):
+    matches = list(re.finditer(r'(?={})'.format(pattern), expressions))    
     if matches:
-        for match in matches:
-            print((match.start(), match.end() + len(sub) - 1))
+        for i in matches:
+            print((i.start(),i.end()+len(pattern)-1))
     else:
-        print((-1, -1))
+        print((-1,-1))  
+def main():
+    express = input()
+    patrs=input()
+    solve(express,patrs) 
+if __name__=='__main__':
+    main()
